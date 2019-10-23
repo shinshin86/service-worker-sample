95bdbaf4ab1fa7096a6ad3ef6bb04784a5a6e2ed
shinshin86 <beagles1986@gmail.com>
2019-10-23 23:20:33 +0900

    Add cache sample

--- sw.js
+++ sw.js
@@ -1,8 +1,31 @@
+const CACHE_NAME = 'my-site-cache-v1';
+const urlsToCache = ['/', '/sample.png'];
+
 self.addEventListener('install', function(event) {
-  console.log('install')
-  console.log('hello world')
+  console.log('===install===');
+
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      console.log({ cache });
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
 self.addEventListener('fetch', function(event) {
+  console.log('===fetch===');
   console.log(event);
-})
+
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      // Cache hit - return response
+      if (response) {
+        console.log({ response });
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
